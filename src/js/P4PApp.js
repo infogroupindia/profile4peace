272f0bf165c54068bf6337a1a3ea3d22b7c759fd
Marc Wagner <marc@blackinfoway.com>
2015-10-27 01:07:54 +0100

    Initial commit

---
+++ src/js/P4PApp.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Nidin Vinayakan on 26-10-2015.
+ */
+var ctx, textImg;
+function startApp() {
+    if (!ctx) {
+        var canvas = document.getElementById("image-canvas");
+        ctx = canvas.getContext("2d");
+        console.log(ctx);
+
+        drawText();
+
+        /*        var data = '<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">' +
+         '<foreignObject width="100%" height="100%">' +
+         '<div style="display: inline;padding: 0;bottom: -142px;left: 7px;position: relative;font-family: \'Ubuntu\', sans-serif;background: rgba(255,244,175,0.88);box-shadow: 5px 0 0 rgba(255,244,175,0.88), -5px 0 0 rgba(255,244,175,0.88);">' +
+         'I\'m an Indian\n' +
+         'I am from Kochi\n' +
+         'I don\'t hate Pakistan\n' +
+         'I\'m not alone\n' +
+         'There are many people\n' +
+         'Like me.' +
+         '</div>' +
+         '</foreignObject>' +
+         '</svg>';
+
+         var DOMURL = window.URL || window.webkitURL || window;
+
+         textImg = new Image();
+         var svg = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+         var url = DOMURL.createObjectURL(svg);
+
+         textImg.onload = function () {
+         ctx.drawImage(textImg, 0, 0, 256, 256);
+         DOMURL.revokeObjectURL(url);
+         };
+
+         textImg.src = url;*/
+    }
+}
+function displayImage(img) {
+    if (!ctx) {
+        startApp();
+    }
+    ctx.drawImage(img, 0, 0, 256, 256);
+    drawText();
+}
+function drawText(){
+    var y = 175;
+
+    ctx.fillStyle = "rgba(255,244,175,0.8)";
+    ctx.fillRect(0, y-20, 256, 256  + 20 - y);
+
+    ctx.fillStyle = "#000000";
+    ctx.font = "italic 1em Ubuntu";
+    ctx.fillText("I'm an Indian", 5, y);
+    ctx.fillText("I'm from Kochi", 5, y + 15);
+    ctx.fillText("I don't hate Pakistan", 5, y + 30);
+    ctx.fillText("I'm not alone", 5, y + 45);
+    ctx.fillText("There are many people", 5, y + 60);
+    ctx.fillText("Like me.", 5, y + 75);
+}
